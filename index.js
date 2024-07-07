1afc036e9d0e7c826e6c715ecd691532a64b7715
Iqra <148074762+Iqraawan451512@users.noreply.github.com>
2024-07-07 09:43:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+/* Q1.Create   a function that accepts two parameters:
+It's perform
+ 
+1.Add
+2.Sub
+3.mul
+4.div  */
+Object.defineProperty(exports, "__esModule", { value: true });
+//add funtion
+function add(a, b) {
+    return a + b;
+}
+console.log(add(3, 4));
+// multiplay
+let multiplay = function (num1, num2) {
+    return num1 * num2;
+};
+console.log(multiplay(3, 4));
+//divide
+let divide = (num1, num2) => {
+    return num1 / num2;
+};
+console.log(divide(20, 2));
+//substract
+function substract(a, b) {
+    return a - b;
+}
+console.log(substract(10, 2));
+/** Assignment
+ Q2.Cities: Write a function called describe_city() that accepts the name
+  of a city and its country. The function should print a simple sentence,
+   such as Karachi is in Pakistan. Give the parameter for the country a
+   default value. Call your function for three different cities, at least
+   one of which is not in the default country.
+*/
+let describe_city = (city1, city2, city3, country = "pakistan") => {
+    return `${city1} , ${city2} and ${city3} are the famous cities of ${country} \n`;
+};
+console.log(describe_city("karachi", "lahore", "islamabad"));
+/* Q3. Create   function that accepts two parameters: and a calculates  Rectangle Area                                                          In this function:
+* width and height are the parameters of type number.
+* The function returns the area, which is the product of width and height
+
+ */
+let areaOfRectangle = (width, height) => {
+    return `the calculated area of Rectrangle is ${width * height}`;
+};
+console.log(areaOfRectangle(12, 4));
